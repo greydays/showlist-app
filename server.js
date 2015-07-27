354d483a34ef9dee2942140b8e460e0256d4eb53
Hana Fallisgaard <hfgaard@gmail.com>
2015-07-27 09:29:58 -0700

    inital server

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
 
-venue auth login
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/users');
+
+var venueRouter = express.Router();
+require('./routes/venue-router')(venueRouter);
+app.use('/venue', venueRouter);
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port);
+});
+
+
+// venue auth login
 // if time, artist login
 // sorting shows based on different criteria
 // past shows should not be displayed

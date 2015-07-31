a0dd950aaa996b307716ca536377b29f2ee4054b
Ryan Flaherty <rcflaherty@gmail.com>
2015-07-31 01:44:08 -0700

    layout left-aligned, bg image smaller, forms and shows cleaned up

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -48,11 +48,11 @@
 	__webpack_require__(9);
 	__webpack_require__(11);
 	__webpack_require__(10);
-	__webpack_require__(15);
-	__webpack_require__(21);
 	__webpack_require__(2);
 	__webpack_require__(19);
 	__webpack_require__(12);
+	__webpack_require__(15);
+	__webpack_require__(21);
 	__webpack_require__(13);
 	__webpack_require__(17);
 	__webpack_require__(18);

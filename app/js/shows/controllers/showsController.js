ad75420fe9d6f5acd11e38a2c42eb27f996054e6
Dylan T. Moberg <dylanmoberg@msn.com>
2015-07-30 17:31:03 -0700

    didnt do anything

--- app/js/shows/controllers/showsController.js
+++ app/js/shows/controllers/showsController.js
@@ -1,26 +1,32 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('showsController', ['$scope', '$http', 'ShowsRESTResource', 'copy', function($scope, $http, resource, copy) {
+  app.controller('showsController', ['$scope', '$http', '$location', 'ShowsRESTResource', 'copy', function($scope, $http, $location, resource, copy) {
     var Show = resource('new-show');
+    var oneShow = resource('show-view'); //***for getting one show?
     $scope.errors = [];
     $scope.show = [];
-    //set up get request to backend
-    // $scope.getShow = function(show) {
-    //   console.log('reached show controller get show');
-    //   console.log(show);
-    //   Show.get(show, function(err, data) {
-    //     if (err) return $scope.errors.push({msg: 'error retrieving show'});
-    //     $scope.show = data;
-    //   });
-    // };
-    var getShow = function() {
+
+    $scope.getAllShows = function() {
       $http.get('/show/shows').success(function(response){
         $scope.shows = response;
       });
     };
 
-    getShow();
+    $scope.getShow = function() {
+      var url = $location.path();
+      url = url.split('/');
+      var id = url[url.length - 1];
+      $http.get('/show/' + id).success(function(response) {
+        $scope.show = response;
+      });
+    };
+
+    $scope.storeShow = function(show) {
+      $http.get('/show/' + show._id).success(function(response) {
+        $scope.show = response;
+      });
+    }
 
     $scope.submitForm = function(show){
       console.log('show', show);
@@ -37,8 +43,20 @@ module.exports = function(app) {
     $scope.destroy = function(id) {
       console.log(id)
       $http.delete('/show/shows/'  + id).success(function(){
-        getShow();
+        $scope.getAllShows();
       });
+    };
+
+//***for linking out from show cards to complete show info
+    $scope.getOneShow = function(show) {
+      $http.get('/:venue/shows/:show').success(function(response){
+        $scope.show = response;
+      });
+    };
+
+    $scope.linkOut = function(id) {
+      console.log(id);
+      $location.path('/show/' + id);
     };
 
   }]);
